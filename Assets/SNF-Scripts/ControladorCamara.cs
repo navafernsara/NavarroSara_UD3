57971181f25bc1f9c5329bfd1ebb7cbcec790c8f
navafernsara <SaraNF@hotmail.es>
2019-11-21 12:51:28 +0100

    SNF - 21/11/19 4to

---
+++ Assets/SNF-Scripts/ControladorCamara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorCamara : MonoBehaviour
+{
+    public GameObject jugador; //objeto a seguir//
+    private Vector3 posicionRelativa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posicionRelativa = transform.position - jugador.transform.position;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = jugador.transform.position + posicionRelativa;
+    }
+}
