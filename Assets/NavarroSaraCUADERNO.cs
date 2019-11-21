ba63b5e50d16005e646860b799231dc17be5d8e7
navafernsara <SaraNF@hotmail.es>
2019-11-21 12:46:51 +0100

    SNF-21-11-19

---
+++ Assets/NavarroSaraCUADERNO.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavarroSaraCUADERNO : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+/* Script GetComponent:
+public class ScriptExample : MonoBehaviour
+{
+    void Start()
+    {
+        // Disable the spring on the HingeJoint component.
+        HingeJoint hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = false;
+    }
+}
+Script ToString:
+//This script changes Text to the name of the GameObject with the script attached
+//Attach this script to a GameObject
+//Attach a Text GameObject in the Inspector (Create>UI>Text)
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Example : MonoBehaviour
+{
+    public Text m_Text;
+
+    private void Start()
+    {
+        //Check that the Text is attached in the Inspector
+        if (m_Text != null)
+            //Change the Text to show the GameObject's name
+            m_Text.text = "GameObject Name : " + gameObject.ToString();
+    }
+}
+*/
