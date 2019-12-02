a23ad583167de47416f9c73e325d84a9150f468f
navafernsara <SaraNF@hotmail.es>
2019-12-02 08:42:34 +0100

    SNF - RollABall 2/12/19
    SNF - RollABall 2/12/19

---
+++ Assets/SNF-Scripts/makeObjectDraggable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class makeObjectDraggable : MonoBehaviour
+{
+    private float dist;
+    private Vector3 v3Offset;
+    private Plane plane;
+
+    void OnMouseDown()
+    {
+        plane.SetNormalAndPosition(Camera.main.transform.forward, transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        plane.Raycast(ray, out dist);
+        v3Offset = transform.position - ray.GetPoint(dist);
+    }
+
+    void OnMouseDrag()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        plane.Raycast(ray, out dist);
+        Vector3 v3Pos = ray.GetPoint(dist);
+        transform.position = v3Pos + v3Offset;
+    }
+}
