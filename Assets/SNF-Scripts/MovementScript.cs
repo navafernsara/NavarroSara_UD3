2354f3f47afac22c732736f160f0b822d05ab1ce
navafernsara <SaraNF@hotmail.es>
2019-12-05 10:34:48 +0100

    SNF - AI_Basico - 05/12/19 - 3

---
+++ Assets/SNF-Scripts/MovementScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject destino;
+
+    private UnityEngine.AI.NavMeshAgent agenteDeNavegacion;
+
+    //Usar esto para iniciar
+    void Start()
+    {
+        agenteDeNavegacion = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        agenteDeNavegacion.SetDestination(destino.transform.position);
+
+    }
+
+}
